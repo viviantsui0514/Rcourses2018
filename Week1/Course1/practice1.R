b7aac2d322448d574ed3dbb1aa03685319eb88da
viviantsui0514 <36395973+viviantsui0514@users.noreply.github.com>
2018-03-07 19:07:15 +0800

    Update practice1.R

--- Week1/Course1/practice1.R
+++ Week1/Course1/practice1.R
@@ -1,20 +1,8 @@
-### practice_1
-### BMI Caculator
-
-# Craete variable called my.height.cm with your actual height in cm 
 my.height.cm <- 161
-
-# Craete variable called my.weight.cm with your actual weight in kg
 my.weight.kg <- 53
-
-# Create my.height.m by my.height.cm 
 my.height.m <- my.height.cm / 100
-
-# Create my.bmi with BMI(Body Mass Index) formula
 my.bmi <- my.weight.kg / (my.height.m) ^ 2
 
-# Use if-else to print matched information
-# Reference: http://www.tpech.gov.taipei/ct.asp?xItem=1794336&CtNode=30678&mp=109171
 if (my.bmi >= 35) {
   print(paste("Your bmi: ", my.bmi))
   print("重度肥胖!")
